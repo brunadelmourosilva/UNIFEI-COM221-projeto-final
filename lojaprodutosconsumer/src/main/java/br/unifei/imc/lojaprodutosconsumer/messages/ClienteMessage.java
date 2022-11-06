db1734434c6f68a1e7e22d466d6bbb9826088b4a
Bruna Delmouro <Bruna.Delmouro-ext@ab-Inbev.com>
2022-11-06 15:34:39 -0300

    Finalizando implementação para envio de e-mail

--- lojaprodutosconsumer/src/main/java/br/unifei/imc/lojaprodutosconsumer/messages/ClienteMessage.java
+++ lojaprodutosconsumer/src/main/java/br/unifei/imc/lojaprodutosconsumer/messages/ClienteMessage.java
@@ -12,4 +12,6 @@ public class ClienteMessage {
     private String name;
 
     private String email;
+
+
 }
--- lojaprodutosconsumer/src/main/java/br/unifei/imc/lojaprodutosconsumer/services/EmailServiceImpl.java
+++ lojaprodutosconsumer/src/main/java/br/unifei/imc/lojaprodutosconsumer/services/EmailServiceImpl.java
@@ -3,11 +3,15 @@ package br.unifei.imc.lojaprodutosconsumer.services;
 import br.unifei.imc.lojaprodutosconsumer.messages.FinalizaPedidoMessage;
 import br.unifei.imc.lojaprodutosconsumer.services.interfaces.EmailService;
 import lombok.extern.log4j.Log4j2;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.Date;
 
 @Service
 @Log4j2
@@ -21,23 +25,29 @@ public class EmailServiceImpl implements EmailService {
 
     @Override
     public void sendEmail(FinalizaPedidoMessage message){
-        SimpleMailMessage sendMessage = prepareSimpleMailMessageFromFinishOrder(message);
-        mailSender.send(sendMessage);
+        if(StringUtils.isNotBlank(message.getCustomer().getEmail())) {
+            SimpleMailMessage sendMessage = prepareSimpleMailMessageFromFinishOrder(message);
+            mailSender.send(sendMessage);
 
-        log.info("E-mail sent!");
+            log.info("E-mail sent to " + message.getCustomer().getEmail());
+        }
     }
 
+
+    //todo adicionar thymeleaf para corpo do e-mail
+    //todo implementar strategy com formas de pagamento escolhida
+    //todo se pix: enviar QR code(aprender como criar QR code)
+    //todo se cartão: enviar dados do cartão de acordo com cliente
     @Override
     public SimpleMailMessage prepareSimpleMailMessageFromFinishOrder(FinalizaPedidoMessage message) {
         var simpleMailMessage =new SimpleMailMessage();
 
         simpleMailMessage.setFrom(sender);
-
-        simpleMailMessage.setTo();
+        simpleMailMessage.setTo(message.getCustomer().getEmail());
 
         simpleMailMessage.setSubject("Dummy subject");
-
         simpleMailMessage.setText("Hello, " + message.getCustomer().getName() + "! ");
+        simpleMailMessage.setSentDate(Date.from(Instant.now()));
 
         return simpleMailMessage;
     }
